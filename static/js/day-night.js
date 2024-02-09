de6d8b5be5f10cdae20fdb18e371b8d65ed9f342
4e6yPek48 <matchtek48@gmail.com>
2024-02-09 18:45:45 +0300

    доделки

--- static/js/day-night.js
+++ static/js/day-night.js
@@ -1,8 +1,6 @@
 const bodyElement = document.querySelector("body");
 
 function updateTheme(isDark) {
-    bodyElement.classList.add("toggle");
-
     setTimeout(() => {
         bodyElement.classList.toggle("dark", isDark);
         bodyElement.classList.toggle("light", !isDark);
@@ -24,26 +22,25 @@ function updateTheme(isDark) {
 document.addEventListener('DOMContentLoaded', function () {
     const toggleCheckbox = document.querySelector(".day-night input");
 
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    const isDarkMode = localStorage.getItem('darkMode') === 'true' || window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (!localStorage.getItem('darkMode')) {
+        localStorage.setItem('darkMode', isDarkMode);
+    }
 
     window.addEventListener('pageshow', function (event) {
-        if (event.persisted || isDarkMode) {
+        if (event.persisted) {
+            const isDarkMode = localStorage.getItem('darkMode') === 'true';
             updateTheme(isDarkMode);
+            toggleCheckbox.checked = isDarkMode;
         }
     });
 
     updateTheme(isDarkMode);
+    toggleCheckbox.checked = isDarkMode;
 
     toggleCheckbox.addEventListener("change", () => {
         const newMode = toggleCheckbox.checked;
         updateTheme(newMode);
-
         localStorage.setItem('darkMode', newMode);
-
-        const iframe = document.getElementById('productIframe');
-        if (iframe) {
-            iframe.contentWindow.postMessage({darkMode: newMode}, '*');
-        }
     });
-
 });
